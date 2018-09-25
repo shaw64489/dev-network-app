a75bc521b5acbfb5e776e962e8e372d6a08f55db
Chris Shaw <shaw64489@yahoo.com>
2018-09-25 09:08:21 -0400

    setup post item actions/reducer - delete post functionality

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -1,5 +1,11 @@
 import axios from 'axios';
-import { ADD_POST, GET_ERRORS } from './types';
+import {
+  ADD_POST,
+  GET_ERRORS,
+  GET_POSTS,
+  POST_LOADING,
+  DELETE_POST
+} from './types';
 
 //ADD post
 export const addPost = postData => dispatch => {
@@ -17,4 +23,48 @@ export const addPost = postData => dispatch => {
         payload: err.response.data
       })
     );
+};
+
+//GET posts
+export const getPosts = () => dispatch => {
+  dispatch(setPostLoading());
+  axios
+    .get('/api/posts')
+    .then(res =>
+      dispatch({
+        type: GET_POSTS,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_POSTS,
+        payload: null
+      })
+    );
+};
+
+//Delete Post
+export const deletePost = id => dispatch => {
+  axios
+    .delete(`/api/posts/${id}`)
+    .then(res =>
+      dispatch({
+        type: DELETE_POST,
+        payload: id
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+//Set Loading state
+export const setPostLoading = () => {
+  return {
+    type: POST_LOADING
+  };
 };
